543d857a4e664e4884e8b6671408729d524547ab
navenaa-p <navenaap21@gmail.com>
2025-08-31 20:51:31 +0530

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const display = document.getElementById('display');
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+// Bonus: Keyboard support
+document.addEventListener('keydown', (e) => {
+  const key = e.key;
+  if (/[0-9+\-*/.]/.test(key)) {
+    append(key);
+  } else if (key === 'Enter') {
+    calculate();
+  } else if (key === 'Backspace') {
+    deleteLast();
+  } else if (key === 'Escape') {
+    clearDisplay();
+  }
+});
